c5a6638fc70132b80d8c1b90cfdf61bf08ecd0eb
Geoff Popple <Geoff.popple@gmail.com>
2016-09-02 07:43:42 +1000

    Section1 webservices complete

--- Assignment2/ZodiacDate.asmx.cs
+++ Assignment2/ZodiacDate.asmx.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Services;
 
 namespace Assignment2
@@ -14,13 +12,13 @@ namespace Assignment2
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
     // [System.Web.Script.Services.ScriptService]
-    public class ZodiacDate : System.Web.Services.WebService
+    public class ZodiacDate : global::System.Web.Services.WebService
     {
-        private Dictionary<ZodiacSign, String> zodiac;
+        private readonly Dictionary<ZodiacSign, String> _zodiac;
 
         public ZodiacDate()
         {
-            zodiac = new Dictionary<ZodiacSign, String>()
+            _zodiac = new Dictionary<ZodiacSign, String>()
             {
                 {new ZodiacSign {DayFrom="21", DayTo="20",MonthFrom="03",MonthTo="04"}, "Aries"},
                 {new ZodiacSign {DayFrom="21", DayTo="21",MonthFrom="04",MonthTo="05"}, "Taurus"},
@@ -37,37 +35,47 @@ namespace Assignment2
             };
         }
         [WebMethod]
-        public string getSign(String month, String day)
+        public string GetSign(String month, String day)
         {
 
-            String output = "Wrong Input Date"; //default output
+            string output = "Wrong Input Date"; //default output
 
-            int monthAsInt = Convert.ToInt32(month);
-            int dayAsInt = Convert.ToInt32(day);
-            
-            if (validateDate(monthAsInt, dayAsInt))
+            int monthAsInt;
+            int dayAsInt ;
 
+            //test to see if the values from the input text can be converted t numbers, if not return invald.
+            bool result=int.TryParse(month,out monthAsInt);
+            bool result2 = int.TryParse(day,out dayAsInt);
+
+            if ((result || result2)==false)
             {
+                return output;
+            }
+
+            //test if integers entered match validate date
+            if (!ValidateDate(monthAsInt, dayAsInt)) return output;
 
             DateTime inputTime = Convert.ToDateTime(dayAsInt + "/" + monthAsInt + (monthAsInt == 1 && dayAsInt <= 20?"/2001":"/2000"));
 
-                foreach (KeyValuePair<ZodiacSign, String> entry in zodiac)
-                {
-                    DateTime fromDate = Convert.ToDateTime(entry.Key.DayFrom + "/" + entry.Key.MonthFrom + "/2000");
-                    DateTime toDate= Convert.ToDateTime(entry.Key.DayTo + "/" + entry.Key.MonthTo +(entry.Key.MonthTo=="01"?"/2001":"/2000"));
+            foreach (KeyValuePair<ZodiacSign, String> entry in _zodiac)
 
-                    if (inputTime.Ticks >= fromDate.Ticks && inputTime.Ticks <= toDate.Ticks)
-                    {
-                        output = entry.Value;
-                        break;
-                    }
-                }
+            {
+                //Create a datetime string from the data in the dictionary.  Acknoldge using a magic number her to
+                //for create a valid date time. Yr2000 chosen as leap yesr.  If the end date month is Jan flick this date
+                //over to 2001 so the date falls between correctly 
+                DateTime fromDate = Convert.ToDateTime(entry.Key.DayFrom + "/" + entry.Key.MonthFrom + "/2000");
+                DateTime toDate= Convert.ToDateTime(entry.Key.DayTo + "/" + entry.Key.MonthTo +(entry.Key.MonthTo=="01"?"/2001":"/2000"));
+
+                //check if my input date falls within the 2 date constructs built above.
+                if (inputTime.Ticks < fromDate.Ticks || inputTime.Ticks > toDate.Ticks) continue;
+                output = entry.Value;
+                break;
             }
-           return output;
+            return output;
            
         }
 
-        private bool validateDate(Int32 month, Int32 day)
+        private static bool ValidateDate(int month, int day)
         {
             //elimate dud months
             if (month <= 0 || month > 12)
@@ -81,7 +89,7 @@ namespace Assignment2
             }
         }
 
-        class ZodiacSign
+        private class ZodiacSign
         {
             public string DayFrom { get; set; }
             public string DayTo { get; set; }
